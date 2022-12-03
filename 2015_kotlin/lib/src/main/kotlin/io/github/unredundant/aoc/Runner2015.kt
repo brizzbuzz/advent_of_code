6f95d3e136903ff0a3f8470fe29b9c8de6a17e7c
Ryan Brink <5607577+unredundant@users.noreply.github.com>
2022-12-03 12:32:59 -0500

    feat(2015): day 07 (#5)

--- 2015_kotlin/lib/src/main/kotlin/io/github/unredundant/aoc/Runner2015.kt
+++ 2015_kotlin/lib/src/main/kotlin/io/github/unredundant/aoc/Runner2015.kt
@@ -3,7 +3,6 @@ package io.github.unredundant.aoc
 import io.github.unredundant.aoc.day.Day01
 import io.github.unredundant.aoc.day.Day02
 import io.github.unredundant.aoc.day.Day03
-import io.github.unredundant.aoc.day.Day04
 import io.github.unredundant.aoc.day.Day05
 import io.github.unredundant.aoc.day.Day06
 import io.github.unredundant.aoc.day.Day07
--- 2015_kotlin/lib/src/main/kotlin/io/github/unredundant/aoc/day/Day07.kt
+++ 2015_kotlin/lib/src/main/kotlin/io/github/unredundant/aoc/day/Day07.kt
@@ -1,16 +1,28 @@
 package io.github.unredundant.aoc.day
 
-object Day07 : Day<Short, Int> {
+object Day07 : Day<UShort, UShort> {
   override val calendarDate: Int = 7
 
-  override fun silver(): Short {
-    val circuits: MutableMap<String, Short> = mutableMapOf()
+  override fun silver(): UShort {
+    val circuits: MutableMap<String, UShort> = mutableMapOf()
     var instructions = input.parseInstruction()
-    return 0
+    return evaluateInstructions(instructions, circuits)
   }
 
-  override fun gold(): Int {
-    TODO("Not yet implemented")
+  override fun gold(): UShort {
+    val circuits: MutableMap<String, UShort> = mutableMapOf("b" to silver())
+    var instructions = input.parseInstruction().filterNot { it.output == "b"  }
+    return evaluateInstructions(instructions, circuits)
+  }
+
+  private fun evaluateInstructions(instructions: List<Instruction>, circuits: MutableMap<String, UShort>): UShort {
+    var instructions = instructions
+    while (instructions.isNotEmpty()) {
+      val (canBeEvaluated, rest) = instructions.partition { it.canBeEvaluated(circuits) }
+      instructions = rest
+      canBeEvaluated.forEach { it.evaluate(circuits) }
+    }
+    return circuits["a"]!!
   }
 
   private enum class Operation {
@@ -27,12 +39,34 @@ object Day07 : Day<Short, Int> {
     val input1: String,
     val input2: String?,
     val output: String,
-  )
+  ) {
+    fun canBeEvaluated(circuit: Map<String, UShort>): Boolean {
+      return when (operation) {
+        Operation.INPUT -> input1.toIntOrNull() != null || circuit.containsKey(input1)
+        Operation.NOT -> circuit.containsKey(input1)
+        Operation.AND, Operation.OR, Operation.LSHIFT, Operation.RSHIFT -> {
+          (input1.toIntOrNull() != null || circuit.containsKey(input1))
+            && (input2?.toIntOrNull() != null || circuit.containsKey(input2))
+        }
+      }
+    }
+
+    fun evaluate(circuit: MutableMap<String, UShort>) {
+      circuit[output] = when (operation) {
+        Operation.INPUT -> input1.toUShortOrNull() ?: circuit[input1]!!
+        Operation.NOT -> circuit[input1]!!.inv()
+        Operation.AND -> (input1.toUShortOrNull() ?: circuit[input1]!!) and(input2?.toUShortOrNull() ?: circuit[input2]!!)
+        Operation.OR -> (input1.toUShortOrNull() ?: circuit[input1]!!) or (input2?.toUShortOrNull() ?: circuit[input2]!!)
+        Operation.LSHIFT -> ((input1.toUShortOrNull() ?: circuit[input1]!!).toInt() shl input2!!.toInt()).toUShort()
+        Operation.RSHIFT -> ((input1.toUShortOrNull() ?: circuit[input1]!!).toInt() shr input2!!.toInt()).toUShort()
+      }
+    }
+  }
 
   private fun String.parseInstruction() = lines().map { line ->
     when {
       line.matches(Regex("NOT \\w+ -> \\w+")) -> {
-        val (input, output) = line.split(" -> ")
+        val (_, input, output) = line.split("NOT ", " -> ")
         Instruction(Operation.NOT, input, null, output)
       }
 
