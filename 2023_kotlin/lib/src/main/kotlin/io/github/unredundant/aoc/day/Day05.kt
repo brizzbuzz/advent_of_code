b9753449095626dd2b7d68632cd0b8835206b7c7
Ryan Brink <5607577+unredundant@users.noreply.github.com>
2023-12-05 20:36:42 -0500

    fix(2023): day 5 perf (#15)

--- 2023_kotlin/lib/src/main/kotlin/io/github/unredundant/aoc/day/Day05.kt
+++ 2023_kotlin/lib/src/main/kotlin/io/github/unredundant/aoc/day/Day05.kt
@@ -12,14 +12,24 @@ object Day05 : Day<Long, Long> {
 
   override fun silver(): Long = sources.fold(silverSeeds) { ids, s -> ids.map { id -> s.translate(id) } }.min()
 
-  override fun gold(): Long = reverseSearch()
+  override fun gold(): Long {
+    var step = 10000L
+    var location = 1L
 
-  private tailrec fun reverseSearch(location: Long = 1): Long {
+    while (step > 0) {
+      location = reverseSearch(location, step)
+      step /= 10
+    }
+
+    return location
+  }
+
+  private tailrec fun reverseSearch(location: Long, step: Long): Long {
     val seed = sources.reversed().fold(location) { acc, source -> source.searchLocation(acc) }
 
     if (goldSeeds.any { it.contains(seed) }) return location
 
-    return reverseSearch(location + 1)
+    return reverseSearch(location + step, step)
   }
 
   private fun Source.searchLocation(location: Long): Long =
