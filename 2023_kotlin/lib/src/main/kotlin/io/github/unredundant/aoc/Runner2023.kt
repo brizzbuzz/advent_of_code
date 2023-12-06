028a097a3883c4ec8cdc23193c50d4ac74d2324c
Ryan Brink <5607577+unredundant@users.noreply.github.com>
2023-12-05 19:45:32 -0500

    feat(2023): day 5 (#14)

--- 2023_kotlin/lib/src/main/kotlin/io/github/unredundant/aoc/Runner2023.kt
+++ 2023_kotlin/lib/src/main/kotlin/io/github/unredundant/aoc/Runner2023.kt
@@ -1,12 +1,10 @@
 package io.github.unredundant.aoc
 
-import io.github.unredundant.aoc.day.Day01
-import io.github.unredundant.aoc.day.Day02
-import io.github.unredundant.aoc.day.Day03
-import io.github.unredundant.aoc.day.Day04
+import io.github.unredundant.aoc.day.*
 import io.github.unredundant.aoc.util.Util.printDayResults
 
 fun main() {
+  printDayResults(Day05)
   printDayResults(Day04)
   printDayResults(Day03)
   printDayResults(Day02)
